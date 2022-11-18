a23c60fd1d7060258c6b7a9a4ab723e5be7d4300
haritiana adriano <hei.adriano.4@gmail.com>
2022-11-18 06:12:37 +0100

    feat: grade model

--- src/main/java/com/note/gestion/model/Grade.java
+++ src/main/java/com/note/gestion/model/Grade.java
@@ -1,4 +1,26 @@
 package com.note.gestion.model;
 
-public class Grade {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Grade implements Serializable {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    private Double average;
 }
