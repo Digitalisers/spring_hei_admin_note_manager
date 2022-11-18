61c198d5886c72ae9e75959eed32883eb47f6bc1
haritiana adriano <hei.adriano.4@gmail.com>
2022-11-18 08:32:17 +0100

    feat: course controller

---
+++ src/main/java/com/note/gestion/controller/CourseController.java
@@ -0,0 +1,40 @@
+package com.note.gestion.controller;
+
+import com.note.gestion.model.Course;
+import com.note.gestion.service.CourseService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+public class CourseController {
+    private CourseService courseService;
+
+    @GetMapping("/course")
+    public List<Course> getAllCourses(
+            @RequestParam(name = "page")int page,
+            @RequestParam(name = "page_size")int pageSize
+    ){
+        return courseService.getAllCourses(page, pageSize);
+    }
+
+    @PostMapping("/course")
+    public Course insertCourse(@RequestParam Course newCourse){
+        return courseService.insertCourse(newCourse);
+    }
+
+    @PutMapping("/course/{id_course}")
+    public Course putModification(
+            @PathVariable(name = "id_course")Long idCourse,
+            @RequestParam Course courseModified
+    ){
+        return courseService.putModification(idCourse, courseModified);
+    }
+
+    @DeleteMapping("/course/{id_course}")
+    public String deleteCourse(@PathVariable(name = "id_course")Long idCourse){
+        return courseService.deleteCourse(idCourse);
+    }
+}
