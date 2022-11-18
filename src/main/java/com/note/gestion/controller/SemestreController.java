201d5d51f2921a02ed27c56f6f5171a77c12baec
haritiana adriano <hei.adriano.4@gmail.com>
2022-11-18 11:42:02 +0100

    feat: semestre controller

---
+++ src/main/java/com/note/gestion/controller/SemestreController.java
@@ -0,0 +1,40 @@
+package com.note.gestion.controller;
+
+import com.note.gestion.model.Semestre;
+import com.note.gestion.service.SemestreService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+public class SemestreController {
+    private SemestreService semestreService;
+
+    @GetMapping("/semestre")
+    public List<Semestre> getAllSemestre(
+            @RequestParam(name = "page")int page,
+            @RequestParam(name = "page_size")int pageSize
+    ){
+        return semestreService.getAllSemestre(page, pageSize);
+    }
+
+    @PostMapping("/semestre")
+    public Semestre insertSemestre(@RequestParam Semestre newSemestre){
+        return semestreService.insertSemestre(newSemestre);
+    }
+
+    @PutMapping("/semestre/{id_semestre}")
+    public Semestre putModification(
+            @PathVariable(name = "id_semestre")Long idSemestre,
+            @RequestParam Semestre semestreModified
+    ){
+        return semestreService.putModification(idSemestre, semestreModified);
+    }
+
+    @DeleteMapping("/semestre/{id_semestre}")
+    public String deleteSemestre(@PathVariable(name = "id_semestre")Long idSemestre){
+        return semestreService.deleteSemestre(idSemestre);
+    }
+}
